f1c8d0756853ac77570cfb9531259080322d4a0a
Hazmi Hafiz <hazmihafiz@gmail.com>
2020-08-18 13:08:10 +0700

    refactor: variable name ctx to c

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -21,7 +21,7 @@ func NewRepository(db *gorm.DB, logger log.Logger) domain.UserRepository {
 }
 
 // Get reads the user with the specified ID from the database.
-func (r repository) Get(ctx context.Context, id uint) (domain.User, error) {
+func (r repository) Get(c context.Context, id uint) (domain.User, error) {
 	user := domain.User{}
 	db := r.db.First(&user, id)
 	return user, db.Error
@@ -29,29 +29,29 @@ func (r repository) Get(ctx context.Context, id uint) (domain.User, error) {
 
 // Create saves a new user record in the database.
 // It returns the ID of the newly inserted user record.
-func (r repository) Create(ctx context.Context, user domain.User) (domain.User, error) {
+func (r repository) Create(c context.Context, user domain.User) (domain.User, error) {
 	db := r.db.Create(&user)
 
 	return user, db.Error
 }
 
 // Update saves the changes to a user in the database.
-func (r repository) Update(ctx context.Context, user domain.User) error {
+func (r repository) Update(c context.Context, user domain.User) error {
 	return nil
 }
 
 // Delete deletes a user with the specified ID from the database.
-func (r repository) Delete(ctx context.Context, id uint) error {
+func (r repository) Delete(c context.Context, id uint) error {
 	return nil
 }
 
 // Count returns the number of the user records in the database.
-func (r repository) Count(ctx context.Context) (int, error) {
+func (r repository) Count(c context.Context) (int, error) {
 	return 0, nil
 }
 
 // Query retrieves the user records with the specified offset and limit from the database.
-func (r repository) Query(ctx context.Context, offset, limit int) ([]domain.User, error) {
+func (r repository) Query(c context.Context, offset, limit int) ([]domain.User, error) {
 	var users []domain.User
 	return users, nil
 }
--- internal/user/service.go
+++ internal/user/service.go
@@ -18,37 +18,37 @@ func NewService(repo domain.UserRepository, logger log.Logger) domain.UserServic
 }
 
 // Get returns the album with the specified the user ID.
-func (s service) Get(ctx context.Context, id uint) (domain.User, error) {
-	user, err := s.repo.Get(ctx, id)
+func (s service) Get(c context.Context, id uint) (domain.User, error) {
+	user, err := s.repo.Get(c, id)
 	return user, err
 }
 
 // Create creates a new album.
-func (s service) Create(ctx context.Context, req domain.User) (domain.User, error) {
-	user, err := s.repo.Create(ctx, req)
+func (s service) Create(c context.Context, req domain.User) (domain.User, error) {
+	user, err := s.repo.Create(c, req)
 	s.logger.Info(err)
 	return user, err
 }
 
 // Update updates the album with the specified ID.
-func (s service) Update(ctx context.Context, id uint, req domain.User) (domain.User, error) {
-	user, err := s.repo.Get(ctx, id)
+func (s service) Update(c context.Context, id uint, req domain.User) (domain.User, error) {
+	user, err := s.repo.Get(c, id)
 	return user, err
 }
 
 // Delete deletes the album with the specified ID.
-func (s service) Delete(ctx context.Context, id uint) (domain.User, error) {
-	user, err := s.Get(ctx, id)
+func (s service) Delete(c context.Context, id uint) (domain.User, error) {
+	user, err := s.Get(c, id)
 	return user, err
 }
 
 // Count returns the number of albums.
-func (s service) Count(ctx context.Context) (int, error) {
-	return s.repo.Count(ctx)
+func (s service) Count(c context.Context) (int, error) {
+	return s.repo.Count(c)
 }
 
 // Query returns the albums with the specified offset and limit.
-func (s service) Query(ctx context.Context, offset, limit int) ([]domain.User, error) {
-	items, err := s.repo.Query(ctx, offset, limit)
+func (s service) Query(c context.Context, offset, limit int) ([]domain.User, error) {
+	items, err := s.repo.Query(c, offset, limit)
 	return items, err
 }
